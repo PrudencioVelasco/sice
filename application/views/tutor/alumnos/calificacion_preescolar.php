baa6fc9f780a570f97e34dda34be13a8985d8f1a
A. Palacio <avelascopalacio@gmail.com>
2020-09-17 12:34:23 -0500

    Modificacion del archivo actual.php y calificacion_preescolar.php para responsividad

--- application/views/tutor/alumnos/calificacion_preescolar.php
+++ application/views/tutor/alumnos/calificacion_preescolar.php
@@ -2,14 +2,15 @@
   <div class="right_col" role="main">
     <div class="">
       <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-12 col-sm-12 col-xs-12">
           <div class="x_panel">
-            <div class="x_title">
+            <div class="x_content">
               <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12 ">
                   <h2><strong><i class="fa fa-check-circle"></i> Significado de calificación</strong></h2>
                 </div>
               </div>
+              <div class="x_title"></div>
               <div class="row">
                 <div class="col-md-3 col-sm-3 col-xs-3 alert alert-success alert-dismissible fade in" role="alert">
                   <strong>E:</strong> Excelente
@@ -24,7 +25,6 @@
                   <strong>NA:</strong> Necesita Apoyo
                 </div>
               </div>
-              <div class="clearfix"></div>
             </div>
             <div class="x_content">
               <div class="row">
